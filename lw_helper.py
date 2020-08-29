d81aa88b57805c5957f32764a61348c29a4884cb
lw-jcardenas <jcardenas@liquidweb.com>
2020-08-29 03:26:38 -0500

    lw auth

--- lw_helper.py
+++ lw_helper.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 import argparse
 import requests, json, pprint
-import sys, os, time, datetime
+import sys, os, time
 import config as c
 
 # Issues:
 # Todo:
-#  Auth
 #  API boilerplate function
 # In progress:
-#  Basic framework
-#  Parser
+#  Auth
 # Done:
+#  Basic framework
+#  Initial parser
 #
 
 def get_args():
@@ -25,9 +25,28 @@ def get_args():
     return args
 
 
-def lw_auth(nocache=False):
+def lw_auth(un=c.un, pw=c.pw, nocache=False):
     """Fetch LW auth token"""
-    pass
+    token_cache = os.path.expanduser(c.token_cache)
+    try:
+        with open(token_cache, 'r') as f:
+            token_data = json.load(f)
+            if token_data['expires'] > time.time():
+                # Yay! we can use the cached token
+                return token_data['token'], token_data['expires']
+    except (IOError, ValueError):
+        # token file doesnt exist, or is empty/expired. time to get a new one
+        pass
+    url = 'https://api.liquidweb.com/bleed/Account/Auth/token'
+    r = requests.get(url, auth=(un, pw))
+    if r.status_code == 200:
+        token_data = r.json()
+        with open(token_cache, 'w') as f:
+            json.dump(token_data, f)
+        return token_data['token'], token_data['expires']
+    else:
+        print("Status Code {} while requesting {}".format(r.status_code, url) )
+        raise Exception("error making api call")
 
 def main():
     """do stuff i guess"""

e82d2313ba238f1ec1b349af8dfcf7074e04c400
jessecardenas <jessecardenas@users.noreply.github.com>
2020-08-31 03:28:49 -0500

    add api boilerplate, and use it

--- lw_helper.py
+++ lw_helper.py
@@ -6,9 +6,9 @@ import config as c
 
 # Issues:
 # Todo:
-#  API boilerplate function
 # In progress:
 # Done:
+#  API boilerplate function
 #  Auth
 #  Basic framework
 #  Initial parser
@@ -25,6 +25,27 @@ def get_args():
     return args
 
 
+def lw_api_request(endpoint=None, un=c.un, token=None, path="/", data=None, method="GET", timeout=10):
+    """Make an api request to liquidweb"""
+    endpoint = endpoint or "https://api.liquidweb.com/bleed/"
+    token = token or lw_auth()[0]
+    path = path.strip(" /")
+    headers = {'Content-Type': 'application/json'}
+    url = '{}{}'.format(endpoint, path)
+    if method == "GET" and data:
+        method = "POST"
+    if data:
+        data = json.dumps(data)
+    if method not in ["GET", "POST", "PUT", "DELETE"]:
+        raise Exception("HTTP method {} not supported".format(method))
+    r = requests.request(method, url, auth=(un, token), data=data, headers=headers, timeout=timeout)
+    if r.status_code in (200, 201, 204):
+        return r
+    else:
+        print("Status Code {} while requesting {}".format(r.status_code, url))
+        raise Exception("error making api call")
+
+
 def lw_auth(un=c.un, pw=c.pw, nocache=False):
     """Fetch LW auth token"""
     token_cache = os.path.expanduser(c.token_cache)
@@ -37,16 +58,21 @@ def lw_auth(un=c.un, pw=c.pw, nocache=False):
     except (IOError, ValueError):
         # token file doesnt exist, or is empty/expired. time to get a new one
         pass
-    url = 'https://api.liquidweb.com/bleed/Account/Auth/token'
-    r = requests.get(url, auth=(un, pw))
-    if r.status_code == 200:
-        token_data = r.json()
-        with open(token_cache, 'w') as f:
-            json.dump(token_data, f)
-        return token_data['token'], token_data['expires']
-    else:
-        print("Status Code {} while requesting {}".format(r.status_code, url) )
-        raise Exception("error making api call")
+    r = lw_api_request(un=c.un, token=pw, path="/Account/Auth/token")
+    token_data = r.json()
+    with open(token_cache, 'w') as f:
+        json.dump(token_data, f)
+    return token_data['token'], token_data['expires']
+    # url = 'https://api.liquidweb.com/bleed/Account/Auth/token'
+    # r = requests.get(url, auth=(un, pw))
+    # if r.status_code == 200:
+    #     token_data = r.json()
+    #     with open(token_cache, 'w') as f:
+    #         json.dump(token_data, f)
+    #     return token_data['token'], token_data['expires']
+    # else:
+    #     print("Status Code {} while requesting {}".format(r.status_code, url) )
+    #    raise Exception("error making api call")
 
 def main():
     """do stuff i guess"""
